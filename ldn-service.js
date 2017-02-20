fa84dd9db27f8a014bf31e92d69e0bb1f39ac75b
Matthew Collier <collier.matthew@gmail.com>
2017-02-20 09:16:22 -0500

    Implement getMessages API.

--- ldn-service.js
+++ ldn-service.js
@@ -25,12 +25,13 @@ function factory($http, $q) {
 
   var service = {};
 
-  service.INBOX_DEFAULT_CONTEXT = {
-    '@context': [
-      CONTEXT_URL_LDP,
-      {owner: {'@id': 'https://w3id.org/security#owner', '@type': '@id'}}
-    ]
-  };
+  service.CONTEXT_URL_AS = CONTEXT_URL_AS;
+  service.CONTEXT_URL_LDP = CONTEXT_URL_LDP;
+
+  service.INBOX_DEFAULT_CONTEXT = [
+    CONTEXT_URL_LDP,
+    {owner: {'@id': 'https://w3id.org/security#owner', '@type': '@id'}}
+  ];
 
   /**
    * Sends a LDN message.
@@ -65,14 +66,14 @@ function factory($http, $q) {
     // `message.target`
     var getInbox;
     if(options.inbox) {
-      getInbox = Promise.resolve(options.inbox);
+      getInbox = $q.when(options.inbox);
     } else {
       getInbox = service.discoverInbox(options.target || message.target);
     }
 
     return getInbox.then(function(inbox) {
-      return $http.post(inbox, JSON.stringify(message), {
-        'Content-Type': 'application/ld+json'
+      return $http.post(inbox, message, {
+        headers: {'Content-Type': 'application/ld+json'}
       });
     });
   };
@@ -115,12 +116,31 @@ function factory($http, $q) {
    *           retrieval of its LDN container information.
    */
   service.getInbox = function(inbox) {
-    return jsonld.promises.frame(inbox, INBOX_FRAME).then(function(framed) {
-      var container = framed['@graph'][0];
-      if(!container) {
-        throw new Error('LDN inbox container not found.');
-      }
-      return new Inbox(container);
+    return $q.when(jsonld.promises.frame(
+      inbox, INBOX_FRAME, {expandContext: {'@base': inbox}, base: null}))
+      .then(function(framed) {
+        var container = framed['@graph'][0];
+        if(!container) {
+          throw new Error('LDN inbox container not found.');
+        }
+        // NOTE: contains can be undefined/string/array
+        if(typeof container.contains === 'undefined') {
+          container.contains = [];
+        }
+        if(typeof container.contains === 'string') {
+          container.contains = [container.contains];
+        }
+        return new Inbox(container);
+      });
+  };
+
+  service.getInboxes = function(url, query) {
+    return $http({
+      method: 'GET',
+      url: url,
+      params: query
+    }).then(function(response) {
+      return response.data;
     });
   };
 
@@ -138,7 +158,8 @@ function factory($http, $q) {
         return inbox;
       }
       // GET target to find inbox relation
-      return jsonld.promises.frame(target, TARGET_FRAME)
+      return $q.when(jsonld.promises.frame(
+        target, TARGET_FRAME, {expandContext: {'@base': target}, base: null}))
         .then(function(framed) {
           var result = framed['@graph'][0];
           if(!result) {
@@ -200,24 +221,26 @@ function factory($http, $q) {
 
     options = angular.extend({
       index: self.messages.length,
-      count: Math.min(self.getMessages.count, self.messages.length)
+      count: Math.min(
+        self.defaults.getMessages.count, self.container.contains.length)
     }, options || {});
 
     var start = options.index;
-    var slice = self.container.slice(start, start + options.count);
+    var slice = self.container.contains.slice(start, start + options.count);
 
     return $q.all(slice.map(function(url, idx) {
-      // TODO: do JSON-LD compaction to CONTEXT_URL
       var frame = {
         '@context': CONTEXT_URL_AS,
         id: url
       };
-      return jsonld.promises.frame(url, frame).then(function(framed) {
-        return {index: idx, message: framed['@graph'][0] || {}};
-      });
+      return jsonld.promises.frame(
+        url, frame, {expandContext: {'@base': url}, base: null})
+        .then(function(framed) {
+          return {index: idx, message: framed['@graph'][0] || {}};
+        });
     })).then(function(results) {
       results.map(function(result) {
-        self.messages[start + result.index] = result;
+        self.messages[start + result.index] = result.message;
       });
       return self;
     });
